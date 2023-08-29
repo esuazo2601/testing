184a6ca7d9ed464ac550d670f99f48604baeaff2
Esteban <esteban.suazo.m2601@gmail.com>
2023-08-28 22:40:56 -0400

    Se crea test que falla en base a la hora actual

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -1,11 +1,18 @@
 from src.main import ohce
 import pytest
 import sys
-
+import datetime
+hora_actual = datetime.datetime.now().hour
 
 @pytest.mark.parametrize("name",["Pedro","Esteban","Gustavo"])
 def test_main1(name, monkeypatch):
     monkeypatch.setattr(sys,"argv",["main.py",name])
-    assert ohce('¡Buenos dias') == f'¡Buenos dias {name}!'
-    assert ohce('¡Buenas tardes') == f'¡Buenas tardes {name}!'
-    assert ohce('¡Buenas noches') == f'¡Buenas noches {name}!'
+    print(hora_actual)
+    if hora_actual >= 6 or hora_actual <= 12:
+        assert ohce() == f'¡Buenos dias {name}!'
+    elif hora_actual > 12 or hora_actual < 20:
+        assert ohce() == f'¡Buenas tardes {name}!'
+    elif hora_actual < 6 or hora_actual >= 20:
+        assert ohce() == f'¡Buenas noches {name}!'
+    else:
+        assert ohce("Hola") == "aloH"
