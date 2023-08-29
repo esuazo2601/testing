ed4dbbca7b4965b8a3782841dfb28f3231429343
Esteban <esteban.suazo.m2601@gmail.com>
2023-08-28 22:46:16 -0400

    Se crea test que falla en base a la hora actual (FIXED)

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import sys
-
+import datetime
+hora_actual = datetime.datetime.now().hour
 def ohce(mensaje):
     return f'{mensaje} {sys.argv[1]}!' 
--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -8,11 +8,9 @@ hora_actual = datetime.datetime.now().hour
 def test_main1(name, monkeypatch):
     monkeypatch.setattr(sys,"argv",["main.py",name])
     print(hora_actual)
-    if hora_actual >= 6 or hora_actual <= 12:
-        assert ohce() == f'¡Buenos dias {name}!'
-    elif hora_actual > 12 or hora_actual < 20:
-        assert ohce() == f'¡Buenas tardes {name}!'
-    elif hora_actual < 6 or hora_actual >= 20:
-        assert ohce() == f'¡Buenas noches {name}!'
-    else:
-        assert ohce("Hola") == "aloH"
+    if hora_actual >= 6 and hora_actual <= 12:
+        assert ohce(hora_actual) == f'¡Buenos dias {name}!'
+    elif hora_actual > 12 and hora_actual < 20:
+        assert ohce(hora_actual) == f'¡Buenas tardes {name}!'
+    else :
+        assert ohce(hora_actual) == f'¡Buenas noches {name}!'
