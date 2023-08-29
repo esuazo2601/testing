f959ff2ed9a9ce5acca95093e46b9db6e185f850
Esteban <esteban.suazo.m2601@gmail.com>
2023-08-28 22:27:35 -0400

    Se crea test para el saludo con los nombres (falla)

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -1,7 +1,15 @@
 from src.main import ohce
+import pytest
+import sys
+
+
 
 def test_main1():
     assert ohce('¡Buenos dias!') == '¡Buenos dias!'
     assert ohce('¡Buenas tardes!') == '¡Buenas tardes!'
     assert ohce('¡Buenas noches!') == '¡Buenas noches!'
 
+@pytest.mark.parametrize("name",["Pedro","Esteban","Gustavo"])
+def test_main2(name, monkeypatch):
+    monkeypatch.setattr(sys,"argv",["main.py",name])
+    assert ohce ('¡Buenos dias!') == f'¡Buenos dias {name}'
