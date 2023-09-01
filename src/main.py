12e759e44247b9ddb68d3fd90582d3717bc8bdf6
Esteban <esteban.suazo.m2601@gmail.com>
2023-09-01 17:01:14 -0400

    Se establece un test para verificar la condición de detención del programa, este falla

--- src/main.py
+++ src/main.py
@@ -1,10 +1,15 @@
 import sys
-import datetime
 
+def main_input():
+    return 0
 def ohce(hora_actual):
-    if hora_actual >= 6 and hora_actual <= 12:
-        return f'¡Buenos días {sys.argv[1]}!'
-    elif hora_actual > 12 and hora_actual < 20:
-        return f'¡Buenas tardes {sys.argv[1]}!'
-    else:
-        return f'¡Buenas noches {sys.argv[1]}!'
+    if  len(sys.argv) > 0:
+        if hora_actual >= 6 and hora_actual <= 12:
+            return f'¡Buenos días {sys.argv[1]}!'
+        elif hora_actual > 12 and hora_actual < 20:
+            return f'¡Buenas tardes {sys.argv[1]}!'
+        else:
+            return f'¡Buenas noches {sys.argv[1]}!'
+    
+    while(1):
+       return 0
--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -1,9 +1,10 @@
-from src.main import ohce
+from src.main import ohce, main_input
 import pytest
 import sys
 import datetime
 hora_actual = datetime.datetime.now().hour
 
+#Test para el saludo inicial
 @pytest.mark.parametrize("name",["Pedro","Esteban","Gustavo"])
 def test_main1(name, monkeypatch):
     monkeypatch.setattr(sys,"argv",["main.py",name])
@@ -14,3 +15,18 @@ def test_main1(name, monkeypatch):
         assert ohce(hora_actual) == f'¡Buenas tardes {name}!'
     else :
         assert ohce(hora_actual) == f'¡Buenas noches {name}!'
+
+#Test para que acabe la ejecución al escribir Stop!
+@pytest.mark.parametrize("name",["Pedro","Esteban","Gustavo"])
+def test_input_function(monkeypatch, capsys, name):
+    # Simula la entrada de datos por teclado
+    user_input = ["Stop!"]
+    monkeypatch.setattr('builtins.input', lambda _: user_input.pop())
+    result = main_input
+
+    # Captura la salida estándar
+    captured = capsys.readouterr()
+
+    # Realiza las aserciones necesarias
+    assert result == f"Adios {name}"
+
